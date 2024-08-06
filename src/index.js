f97df23b3ea6b090c994da5746be0e0882b28529
oOtroyOo <497790325@qq.com>
2024-08-06 20:52:20 +0800

    https

--- src/index.js
+++ src/index.js
@@ -2,22 +2,23 @@ import Server from "./nodejs_http_server/server.js"
 import * as requestHandlers from "./nodejs_http_server/requestHandlers.js"
 import * as proxy from "./proxy.js"
 import http from "http"
+import https from "https"
 import Koa from 'koa';
-import Router from 'koa-router';
 import serve from 'koa-static';
+import Router from 'koa-router';
 import mount from 'koa-mount';
 import convert from 'koa-convert';
 import serveIndex from 'koa-serve-index'
-import co from 'co'
-import c2k from 'koa-connect'
+import sslify from 'koa-sslify'
+import fs from "fs";
 
+let port = 8880
 process.argv.forEach((val, index) => {
     console.log(`${index}: ${val}`);
     if (val.startsWith("--port")) {
         var split = val.indexOf('=')
         if (split > 0) {
-            var port = parseInt(val.substring(split + 1))
-            Server.port = port
+            port = parseInt(val.substring(split + 1))
         }
     }
 });
@@ -25,15 +26,6 @@ process.argv.forEach((val, index) => {
 
 const app = new Koa();
 const router = Router();
-
-// Example Connect middleware
-const connectMiddleware = (req, res, next) => {
-    console.log('Connect middleware triggered');
-    next();
-};
-
-app.use(c2k(connectMiddleware));
-
 
 /* 中间件 */
 app.use(async (ctx, next) => {
@@ -50,7 +42,19 @@ Object.keys(requestHandlers).forEach(function (key) {
 app.use(router.routes());
 app.use(mount('/file', convert(serveIndex('./', { icons: true }))));
 app.use(mount('/file', serve('./', { icons: true })));
-let server = app.listen(8880);
+
 // const server = http.createServer();
-// server.listen(8880);
-server.addListener('connect', proxy._onConnect)
+// server.listen(port);
+app.listen(port)
+    .addListener('connect', proxy._onConnect)
+
+if (fs.existsSync('ssl/server.key') && fs.existsSync('ssl/server.key')) {
+    app.use(sslify.default({
+    }))
+    https.createServer({
+        key: fs.readFileSync('ssl/server.key'),
+        cert: fs.readFileSync('ssl/server.crt')
+    }, app.callback())
+        .addListener('connect', proxy._onConnect)
+        .listen(port + 1);
+}
