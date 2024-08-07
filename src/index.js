73525e8e3500c3f2dacf525620c249ad5368a4e1
oOtroyOo <497790325@qq.com>
2024-08-07 17:33:59 +0800

    优化

--- src/index.js
+++ src/index.js
@@ -40,31 +40,49 @@ if (fs.existsSync('ssl/server.key') && fs.existsSync('ssl/server.key')) {
 
 /* 中间件 */
 app.use(async (ctx, next) => {
+
     try {
         console.log('start ' + ctx.method + ":" + ctx.URL);
-        await next();
-        console.log('end ' + ctx.method + ":" + ctx.URL);
+        await next(); // 插入等待后续接口
     } catch (e) {
-        // 如果后面的代码报错 返回500
         ctx.status = 500
         ctx.body = e.stack
         // e.expose = true
         // ctx.onerror(e)
         console.error(`\n${e.stack.replace(/^/gm, '  ')}\n`);
+    } finally {
+        console.log('end ' + ctx.method + ":" + ctx.URL);
     }
 });
+
+// 通用处理
+app.use(async (ctx, next) => {
+    var language = ctx.acceptsLanguages()
+    if (language && language.length > 0) {
+        ctx.response.set('content-language', language[0])
+    }
+
+    var charset = ctx.acceptsCharsets()
+    if (charset && charset.length > 0) {
+    }
+
+    var encoding = ctx.acceptsEncodings()
+    if (encoding && encoding.length > 0) {
+    }
+    await next()
+})
 
 /* 路由部分 */
 router.all('/', requestHandlers.hello);
 Object.keys(requestHandlers).forEach(function (key) {
     router.all('/' + key, requestHandlers[key])
 })
-app.use(router.routes());
-// app.use(mount('/file', convert(serveIndex('./', { icons: true }))));
-// app.use(mount('/file', serve('./', { icons: true })));
 
-// const server = http.createServer();
-// server.listen(port);
+app.use(router.routes());
+
+app.use(mount('/file', convert(serveIndex('./', { icons: true }))));
+app.use(mount('/file', serve('./', { icons: true })));
+
 app.listen(port)
     .addListener('connect', proxy._onConnect)
 
