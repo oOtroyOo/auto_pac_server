df56a05e544134010e28e75af92f65fd6a8b90ae
oOtroyOo <497790325@qq.com>
2024-12-19 12:19:41 +0800

    动态读取代码

--- src/proxy.js
+++ src/proxy.js
@@ -1,23 +1,47 @@
-
 import * as net from "net";
+import fs from "fs";
+import vm from "vm"; // Import vm to run the script
 
 var regex_hostport = /^([^:]+)(:([0-9]+))?$/;
 
 
 //https://stackoverflow.com/questions/8165570/https-proxy-server-in-node-js
 
-export function _onConnect(req, socket, bodyhead) {
+export async function _onConnect(req, socket, bodyhead) {
     var hostPort = _getHostPortFromString(req.url, 443);
     var hostDomain = hostPort[0];
     var port = parseInt(hostPort[1]);
     console.log("Proxying HTTPS request for:", hostDomain, port);
 
+    // Read and load the PAC script
+    const pacScript = fs.readFileSync("./bin/pac_127.0.0.1.js", "utf8");
+    const script = new vm.Script(pacScript);
+    const sandbox = {
+        FindProxyForURL: null,
+        dnsDomainIs: function (host, domain) {
+            if (domain.startsWith("*.")) {
+                domain = domain.substring(2);
+                return host.endsWith(domain) && (host.length === domain.length || host.charAt(host.length - domain.length - 1) === '.');
+            } else if (host.endsWith(domain)) {
+                return true
+            }
+            return host === domain;
+        }
+    };
+    script.runInNewContext(sandbox);
+    const FindProxyForURL = sandbox.FindProxyForURL;
+
+    // Check if the request host address is a valid proxy server
+    var proxy = FindProxyForURL(req.url, hostDomain);
+    if (proxy && proxy.startsWith("SOCKS5")) {
+
+    }
+
     var proxySocket = new net.Socket();
     proxySocket.connect(port, hostDomain, function () {
         proxySocket.write(bodyhead);
         socket.write("HTTP/" + req.httpVersion + " 200 Connection established\r\n\r\n");
-    }
-    );
+    });
 
     proxySocket.on('data', function (chunk) {
         socket.write(chunk);
@@ -27,7 +51,8 @@ export function _onConnect(req, socket, bodyhead) {
         socket.end();
     });
 
-    proxySocket.on('error', function () {
+    proxySocket.on('error', function (err) {
+        console.error("Proxy socket error:", err);
         socket.write("HTTP/" + req.httpVersion + " 500 Connection error\r\n\r\n");
         socket.end();
     });
@@ -40,7 +65,8 @@ export function _onConnect(req, socket, bodyhead) {
         proxySocket.end();
     });
 
-    socket.on('error', function () {
+    socket.on('error', function (err) {
+        console.error("Client socket error:", err);
         proxySocket.end();
     });
 }
