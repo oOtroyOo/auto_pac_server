4f824a033587d4a146a354b7905c87c64f3b9803
oOtroyOo <497790325@qq.com>
2026-01-19 11:44:23 +0800

    --lfs-root

--- run_linux.sh
+++ run_linux.sh
@@ -1,2 +1,2 @@
 cd `dirname $0`
-node --es-module-specifier-resolution=node src/index.js --input-type=module --port=8880
+node --es-module-specifier-resolution=node src/index.js --input-type=module --port=8880 --lfs-root=/volume1/工作文件夹/git-repo/lfs
--- src/controllers/LfsController.js
+++ src/controllers/LfsController.js
@@ -2,6 +2,8 @@
 import Koa from 'koa';
 import path from 'path';
 import koa_local_git_lfs from 'koa-local-git-lfs'
+import fs from 'fs';
+
 // 参考文档：
 // https://github.com/git-lfs/git-lfs/blob/main/docs/api/server-discovery.md
 // https://github.com/git-lfs/git-lfs/blob/main/docs/api/batch.md
@@ -11,7 +13,21 @@ import koa_local_git_lfs from 'koa-local-git-lfs'
 export default class LfsController extends BaseController {
     constructor(app, router) {
         super(app, router);
-        const lfs = new koa_local_git_lfs(path.resolve(process.cwd(), 'lfs_objects'))
+        let folder = path.resolve(process.cwd(), 'lfs_objects')
+        process.argv.forEach((val, index) => {
+            if (val.startsWith("--lfs-root")) {
+                var split = val.indexOf('=')
+                if (split > 0) {
+                    folder = val.substring(split + 1)
+                }
+            }
+        });
+        console.log("LFS root folder: " + folder)
+
+        if (!fs.existsSync(folder)) {
+            fs.mkdirSync(folder)
+        }
+        const lfs = new koa_local_git_lfs(folder)
         router.use("/lfs", lfs.routes())
     }
 
